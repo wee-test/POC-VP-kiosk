14e8952c1f50161e28076064b6d35b2e3657babd
NguyenKhang <khang@wee.vn>
2021-01-20 16:27:39 +0700

    add ui call loading

---
+++ Sample/app/src/main/java/wee/digital/sample/ui/fragment/member/CallFragment.kt
@@ -0,0 +1,16 @@
+package wee.digital.sample.ui.fragment.member
+
+import wee.digital.sample.R
+import wee.digital.sample.ui.main.MainFragment
+
+class CallFragment : MainFragment() {
+
+    override fun layoutResource(): Int = R.layout.fragment_call
+
+    override fun onViewCreated() {
+
+    }
+
+    override fun onLiveDataObserve() {}
+
+}
