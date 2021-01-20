e41e9d969984a75e221b0394e5549c4903b998e7
NguyenKhang <khang@wee.vn>
2021-01-20 13:49:10 +0700

    update ui document

---
+++ Sample/app/src/main/java/wee/digital/sample/ui/fragment/review/ReviewFragment.kt
@@ -0,0 +1,14 @@
+package wee.digital.sample.ui.fragment.review
+
+import wee.digital.sample.R
+import wee.digital.sample.ui.main.MainFragment
+
+class ReviewFragment : MainFragment() {
+
+    override fun layoutResource(): Int = R.layout.review
+
+    override fun onViewCreated() {}
+
+    override fun onLiveDataObserve() {}
+
+}
