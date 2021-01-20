f4678a260c8367801eca42a2afa3a08a874712cd
NguyenKhang <khang@wee.vn>
2021-01-20 17:01:26 +0700

    add ui fail

---
+++ Sample/app/src/main/java/wee/digital/sample/ui/fragment/member/FailFragment.kt
@@ -0,0 +1,14 @@
+package wee.digital.sample.ui.fragment.member
+
+import wee.digital.sample.R
+import wee.digital.sample.ui.main.MainFragment
+
+class FailFragment : MainFragment(){
+
+    override fun layoutResource(): Int = R.layout.fail
+
+    override fun onViewCreated() {}
+
+    override fun onLiveDataObserve() {}
+
+}
