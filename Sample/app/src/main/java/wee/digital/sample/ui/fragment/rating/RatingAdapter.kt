4c20598df7ec2c0cfe18d94b5d0683f70a10a336
KhanhNguyen <khanhnguyen@wee.vn>
2021-01-21 14:57:11 +0700

    update rating

--- Sample/app/src/main/java/wee/digital/sample/ui/fragment/rating/RatingAdapter.kt
+++ Sample/app/src/main/java/wee/digital/sample/ui/fragment/rating/RatingAdapter.kt
@@ -2,32 +2,34 @@ package wee.digital.sample.ui.fragment.rating
 
 import android.view.View
 import kotlinx.android.synthetic.main.rating_item.view.*
-import wee.digital.library.extension.bold
-import wee.digital.library.extension.drawable
-import wee.digital.library.extension.regular
+import wee.digital.library.adapter.BaseRecyclerAdapter
 import wee.digital.sample.R
-import wee.digital.sample.ui.fragment.dialog.selectable.SelectableAdapter
 
-/*
-class RatingAdapter : SelectableAdapter<RatingItem>() {
+class RatingAdapter : BaseRecyclerAdapter<RatingItem>() {
+
+    var onItemText: (RatingItem) -> String = { it.toString() }
+
+    var onItemSelected: (RatingItem, Int) -> Unit = { _, _ -> }
+
+    override var onItemClick: (RatingItem, Int) -> Unit = { model, position ->
+        notifyDataSetChanged()
+        super.onItemClick(model, position)
+        onItemSelected(model,position)
+    }
 
     override fun layoutResource(model: RatingItem, position: Int): Int {
         return R.layout.rating_item
     }
 
-    override fun View.onBindModel(model: RatingItem) {
-        model.reactIcon?.let { imageViewReact.setImageResource(it) }
+    override fun View.onBindModel(model: RatingItem, position: Int, layout: Int) {
+        model.reactIcon?.let { imageViewReact.setBackgroundResource(it) }
         textViewReact.text = model.reactContent
+        if (model.isSelected) {
+            textViewReact.setTextColor(context.getColor(R.color.green))
+            imageViewReact.isChecked = true
+        } else {
+            textViewReact.setTextColor(context.getColor(R.color.black))
+            imageViewReact.isChecked = false
+        }
     }
-
-    override fun View.onBindModelSelected(model: RatingItem) {
-        backgroundReact.background = drawable(R.drawable.bg_react)
-        textViewReact.bold()
-    }
-
-    override fun View.onBindModelUnselected(model: RatingItem) {
-        backgroundReact.background = null
-        textViewReact.regular()
-    }
-
-}*/
+}
--- Sample/app/src/main/java/wee/digital/sample/ui/fragment/rating/RatingItem.kt
+++ Sample/app/src/main/java/wee/digital/sample/ui/fragment/rating/RatingItem.kt
@@ -4,15 +4,16 @@ import wee.digital.library.extension.string
 import wee.digital.sample.R
 
 class RatingItem(
-        var reactIcon: Int? = R.drawable.ic_rating_love_select,
-        var reactContent: String? = ""
+        var reactIcon: Int? = R.drawable.ic_rating_love,
+        var reactContent: String? = "",
+        var isSelected : Boolean = false
 ) {
     companion object {
-        val defaultList = listOf(
-                RatingItem(R.drawable.ic_rating_love_select, string(R.string.great)),
-                RatingItem(R.drawable.ic_rating_smile_select, string(R.string.look_good)),
-                RatingItem(R.drawable.ic_rating_neutral_select, string(R.string.improve)),
-                RatingItem(R.drawable.ic_rating_thinking_select, string(R.string.hard))
+        val defaultList = arrayListOf(
+                RatingItem(R.drawable.ic_rating_love, string(R.string.great), true),
+                RatingItem(R.drawable.ic_rating_smile, string(R.string.look_good), false),
+                RatingItem(R.drawable.ic_rating_neutral, string(R.string.improve), false),
+                RatingItem(R.drawable.ic_rating_thinking, string(R.string.hard), false)
         )
     }
 }
